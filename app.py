68b58d612e323d12db85bbe13ba9b4be47fb2134
diawando <abduldiawade777@gmail.com>
2023-03-12 17:27:33 +0000

    config de base du projet, ajout du fichier app.py, creation du model Employee, .gitignore et inscription du folder env.

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:lavie@localhost:5432/entreprise'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    hire_date = db.Column(db.Date, nullable=False)
+    active = db.Column(db.Boolean, nullable=False)
+    
+    def __repr__(self):
+        return f'<Employee {self.firstname} {self.lastname}>'
